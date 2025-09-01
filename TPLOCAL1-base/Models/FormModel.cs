0ec38af0d903a547d54d4efc832b615d393b03b6
HMansi5 <spirits5.d.d@gmail.com>
2025-09-01 02:10:06 +0200

    Add files via upload

---
+++ TPLOCAL1-base/Models/FormModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TPLOCAL1.Models
+{
+    public class FormModel
+    {
+        [Required(ErrorMessage = "Champ obligatoire")]
+        public string Nom { get; set; }
+
+        [Required(ErrorMessage = "Champ obligatoire")]
+        public string Prenom { get; set; }
+
+        [Required(ErrorMessage = "Champ obligatoire")]
+        public string Genre { get; set; }
+
+        [Required(ErrorMessage = "Champ obligatoire")]
+        public string Adresse { get; set; }
+
+        [Required(ErrorMessage = "Champ obligatoire")]
+        public string CodePostal { get; set; }
+
+        [Required(ErrorMessage = "Champ obligatoire")]
+        public string Ville { get; set; }
+
+        [Required(ErrorMessage = "Champ obligatoire")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Champ obligatoire")]
+        [DataType(DataType.Date)]
+        public DateTime DateDebut { get; set; }
+
+        [Required(ErrorMessage = "Champ obligatoire")]
+        public string Formation { get; set; }
+        [Required(ErrorMessage = "Champ obligatoire")]
+        public string Aviscobol { get; set; }
+        [Required(ErrorMessage = "Champ obligatoire")]
+        public string Aviscsharp { get; set; }
+    }
+}
---
+++ TPLOCAL1-base/Views/Home/listeavis.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TPLOCAL1.Views.Home
+{
+    public class listeavisModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
